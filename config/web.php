724524bfaf0f0266651d8e678c9001eddd8de5bc
Ivan Nikiforov <vanassmeister@gmail.com>
2016-04-12 20:08:58 +0300

    Подключил urlManager и authManager

--- config/web.php
+++ config/web.php
@@ -38,14 +38,15 @@ $config = [
             ],
         ],
         'db' => require(__DIR__ . '/db.php'),
-        /*
         'urlManager' => [
             'enablePrettyUrl' => true,
             'showScriptName' => false,
             'rules' => [
             ],
         ],
-        */
+        'authManager' => [
+            'class' => 'yii\rbac\DbManager',
+        ],        
     ],
     'params' => $params,
 ];
